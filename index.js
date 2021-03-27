2a9c06249a73612df66101260d47e396bc0f56f6
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-03-26 20:26:57 -0700

    create questions

--- index.js
+++ index.js
@@ -4,7 +4,60 @@ const inquirer = require('inquirer');
 const generateMarkDown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username? (Required)',
+        validate: userName => {
+            if (userName) {
+                return true;
+            } else {
+                console.log('Please enter your user name');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name:'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project (Required)',
+        validate: userDescription => {
+            if (userDescription) {
+                return true;
+            } else {
+                console.log('Please enter project description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the purpose of this repo?'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What license are you using for this project?',
+        choices: ['MIT', 'Apache2.0', 'ISC', 'GNUGOLv3', 'none']
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Who is the contributor for this project?'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
