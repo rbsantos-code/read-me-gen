26d2ace4dff5efc51088c098d17970df475a3887
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-03-27 21:48:18 -0700

    fix github link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,7 +24,7 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
   ## GitHub Deployable URL
-  * [${data.username}]{https://github.com/${data.username}/}
+  * [${data.username}](https://github.com/${data.username}/)
 
   ## Description
   * ${data.description}
